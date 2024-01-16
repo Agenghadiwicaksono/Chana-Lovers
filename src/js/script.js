b587a6d968e7b1099c1cfddf556b03fe9fa91978
agenghw27@gmail.coml <agenghadiwicaksono@MacBook-Pro-Ageng.local>
2024-01-16 08:07:07 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//navbarfixed
+window.onscroll = function (){
+    const header = document.querySelector('header');
+    const navbarfixed = header.offsetTop;
+
+    if (window.pageYOffset) {
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+//button humberger
+const humberger = document.querySelector('#humberger');
+const navmenu = document.querySelector('#nav-menu');
+ humberger.addEventListener('click',function(){
+    humberger.classList.toggle('active');
+    navmenu.classList.toggle('hidden');
+ })
