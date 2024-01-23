a8b0bca0f036ceacb83d127f02cfb27293d93b85
agenghw27@gmail.coml <agenghadiwicaksono@MacBook-Pro-Ageng.local>
2024-01-23 10:27:36 +0700

    third-commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = function (){
     const header = document.querySelector('header');
     const navbarfixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
 
-    if (window.pageYOffset) {
+    if (window.pageYOffset > navbarfixed) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -16,4 +19,33 @@ const navmenu = document.querySelector('#nav-menu');
  humberger.addEventListener('click',function(){
     humberger.classList.toggle('active');
     navmenu.classList.toggle('hidden');
- })
+ });
+
+//  klik dimana pun
+window.addEventListener('click', function(a){
+    if (a.target !=humberger && a.target !=navmenu) {
+        humberger.classList.remove('active');
+        navmenu.classList.add('hidden');
+    }
+});
+
+// darkmode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click',function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// perpindah tombol
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  }else{
+    darkToggle.checked = false;
+  }
